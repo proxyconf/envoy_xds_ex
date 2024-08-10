20677a9800075e1d66734bedccefded26a7fe1f2
Andre Graf <andre@dergraf.org>
2024-08-10 21:44:11 +0200

    added envoy httpuri

---
+++ lib/envoy/config/core/v3/http_uri.pb.ex
@@ -0,0 +1,11 @@
+defmodule Envoy.Config.Core.V3.HttpUri do
+  @moduledoc false
+
+  use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+
+  oneof :http_upstream_type, 0
+
+  field :uri, 1, type: :string, deprecated: false
+  field :cluster, 2, type: :string, oneof: 0, deprecated: false
+  field :timeout, 3, type: Google.Protobuf.Duration, deprecated: false
+end
